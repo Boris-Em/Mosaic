580b3eafb493e2f964f4c80ed1deaf1a93dbad4e
Boris Emorine <boris.emorine@gmail.com>
2019-10-21 22:25:24 +0200

    Fiz `PoolTileMapper`
    The `PoolTileMapper` was using `Int`s which would give us rounded values and everything was just pure colors.
    This commit also adds a safe check.

--- Mosaic Sample App/Mosaic/Pipeline/PoolTileMapper.swift
+++ Mosaic Sample App/Mosaic/Pipeline/PoolTileMapper.swift
@@ -20,15 +20,18 @@ final class PoolTileMapper {
     }
     
     func imagePositions(for sequence: ImageTileSequence, of averageColors: [UInt16]) -> [ImagePositionValuePair] {
+        guard sequence.count == averageColors.count / 4 else {
+            fatalError("The number of average colors and tiles don't match. \n There are \(sequence.count) tiles in the sequence, but \(averageColors.count / 4) average colors.")
+        }
         
         var tileImagePositions = [ImagePositionValuePair]()
         
         for (index, frame) in sequence.enumerated() {
-            let red = averageColors[index * 4]
-            let green = averageColors[index * 4 + 1]
-            let blue = averageColors[index * 4 + 2]
+            let red = Double(averageColors[index * 4])
+            let green = Double(averageColors[index * 4 + 1])
+            let blue = Double(averageColors[index * 4 + 2])
             let alpha = averageColors[index * 4 + 3]
-            let averageColor = UIColor(r: CGFloat(red / 255), g: CGFloat(green / 255), b: CGFloat(blue / 255), a: CGFloat(alpha))
+            let averageColor = UIColor(r: CGFloat(red / 255.0), g: CGFloat(green / 255.0), b: CGFloat(blue / 255.0), a: CGFloat(alpha))
             
             let closestTileImage = poolManager.closestImage(from: averageColor)
             let closestTileResizedImage = resizedImageManager.resizedImage(for: closestTileImage, size: sequence.tileSize)
